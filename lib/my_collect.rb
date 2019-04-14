0d34aa84f30be898bb0f127ec5a735580ab704cd
Janice Pena <jipena94@gmail.com>
2019-04-14 00:27:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,6 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(array)
   i = 0
- 
   while i < array.length
     yield(array[i])
     i = i + 1
@@ -14,7 +13,6 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(collection)
   i = 0
-  collection = []
   while i < collection.length
     yield(collection[i])
     i = i + 1
