fe29c9ec356f256c00df745944ab84d264abc304
Janice Pena <jipena94@gmail.com>
2019-04-14 00:20:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
-
+def my_collect(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+my_collect(array) {|i| i.split(" ").first}
+end
